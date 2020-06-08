c1571379c269798ffcf2969e4fc615e47bdac669
Patryk <patryk.ties@lovehomeswap.com>
2020-06-08 08:48:38 +0100

    env test2

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>secret is {process.env.secret}</p>
+        <p>secret is {process.env.SECRET_KEY} woot!</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
